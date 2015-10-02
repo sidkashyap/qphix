bf125730015a4843fe2db8ea779d6ef99bb824b8
Sidharth N. Kashyap <sid@alchemy.local>
2015-10-02 16:15:04 +0100

    test123

---
+++ build_one/tests/CMakeFiles/time_dslash_noqdp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sid/Work/qphix/tests/timeDslashNoQDP.cc" "/Users/sid/Work/qphix/build_one/tests/CMakeFiles/time_dslash_noqdp.dir/timeDslashNoQDP.cc.o"
+  "/Users/sid/Work/qphix/tests/time_dslash_noqdp.cc" "/Users/sid/Work/qphix/build_one/tests/CMakeFiles/time_dslash_noqdp.dir/time_dslash_noqdp.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QPHIX_AVX_SOURCE=1"
+  "QPHIX_LLC_CACHE_ALIGN=64"
+  "QPHIX_SCALAR=1"
+  "QPHIX_SOALEN=8"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../tests/../include"
+  "../tests/../build/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/sid/Work/qphix/build_one/lib/CMakeFiles/qphix_solver.dir/DependInfo.cmake"
+  )
